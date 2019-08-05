96b0a9ae3f2f7b2de3ceef97ddd2ca658713a6f2
leemyunggyu <jaylee@hatiolab.com>
2019-08-05 14:03:03 +0800

    v0.0.11

--- client/components/search-form.js
+++ client/components/search-form.js
@@ -63,7 +63,7 @@ class SearchForm extends LitElement {
                 `
               : html`
                   <label ?hidden=${field.hidden}>${field.name || field.id}</label>
-                  <input .type=${field.type} .value=${field.value} .name=${field.name} ?hidden=${field.hidden} />
+                  <input .type=${field.type} .value=${field.valu || ''} .name=${field.name} ?hidden=${field.hidden} />
                 `}
           `
         )}
